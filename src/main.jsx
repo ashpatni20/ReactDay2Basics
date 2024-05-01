221bf0a66521cb9cf51fe4bb3997d4a12c4eac53
ashpatni20 <ash.patni20@gmail.com>
2024-05-01 22:07:30 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+import ReacTags from './components/ReactTags.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <ReacTags></ReacTags>
+
+)
